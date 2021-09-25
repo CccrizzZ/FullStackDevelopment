64e96fac9e6f6f076f113459e39535c1f5f0cb79
cccrizzz <ccccrizzzz@gmail.com>
2021-09-25 23:55:14 +0800

    Lab7fixed

--- Lab7.2/expressgenerator/myapp/routes/users.js
+++ Lab7.2/expressgenerator/myapp/routes/users.js
@@ -6,14 +6,23 @@ var bodyParser = require('body-parser')
 var router = express.Router()
 router.use(bodyParser.urlencoded({ extended: true }))
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource')
+  
 })
 
+
 router.post('/', (req, res, next) => {
-  res.send('posting')
+
+  res.send("POST received")
+  
+
+  console.log("First name: " + req.query.firstname)
+  console.log("Last name: " + req.query.lastname)
+
 })
+
+
 
 
 
